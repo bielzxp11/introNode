2b3928c5a3f114490df4d450e00537d579f68d55
bielzxp11 <134931346+bielzxp11@users.noreply.github.com>
2024-08-19 11:43:35 -0300

    aula2 19/08

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express'); 
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const router = require('./routes/routes')
+const router = require('./routes/routes');
 
-const app = express(); 
-
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
 // const porta = process.env.PORT || 3333;
 const porta = 3333;
 
 app.listen(porta, () => {
-    console.log('Servidor iniciado na porta ' + porta); 
+    console.log('Servidor iniciado na porta ' + porta);
     //console.log(`Servidor iniciado na porta ${porta}`);
 });
 
 app.get('/', (request, response) => {
-    response.send('');
-})
+    response.send('oi');
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
 
 // referência a controllers que serão utilizados nas rotas
-const UsuariosController = require('../controllers/usuarios'); 
+const UsuariosController = require('../controllers/usuarios');
 
 // definição das rotas
-router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.get('/usuarios', UsuariosController.listarUsuarios);
+router.post('/usuarios', UsuariosController.cadastrarUsuarios);
+router.patch('/usuarios', UsuariosController.editarUsuarios);
+router.delete('/usuarios', UsuariosController.apagarUsuarios);
 // post
 // patch
 // delete
